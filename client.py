422ecffa71212e6dcba9bc0f66cfa343c4cdbc6f
david go <david.go@us.af.mil>
2025-01-15 09:22:55 -0600

    client

--- client.py
+++ client.py
@@ -1,12 +1,41 @@
 import socket
 import datetime
 
-
 def stamp(message):
     ct = datetime.datetime.now()
-    print(message)
+    # print(message)
     with open("log.txt", "a") as file:
         file.write(f"{ct}: {message}\n")
+
+def sendmessage(client_socket, data):
+    # client_socket.send(combination.encode())
+    client_socket.send(data.encode())
+    stamp(f"Sent to server: {data}")
+
+def upload(client_socket):
+    try: 
+        # Reading file and sending data to server 
+        fi = open("upload.txt", "r") 
+        data = fi.read() 
+        if not data: 
+            return
+        while data: 
+            client_socket.send(str(data).encode()) 
+            data = fi.read() 
+        # File is closed after data is sent 
+        fi.close() 
+
+    except IOError: 
+        print("Error")
+
+def recvmessage(client_socket):
+    data = client_socket.recv(1024)  # Buffer size is 1024 bytes
+    if not data:
+        stamp("No response received from the server.")
+        raise ValueError("No response received from the server.")
+    print(data)
+    stamp(f"Received from server: {data.decode()}\n")
+    
 
 def start_client():
     # Step 1: Create a socket object
@@ -22,22 +51,33 @@ def start_client():
         # message = f"Connected to server at {host}:{port}"
         stamp(f"Connected to server at {host}:{port}")
         
-        # Step 3: Send data to the server
+        # authenticate
         id = input("ID: ")
         password = input("Password: ")
         combination = f"{id} {password}"
-        client_socket.send(combination.encode())
-        exit = input("Type 'shutdown' to exit: ")
-        client_socket.send(exit.encode())
+        # send
+        sendmessage(client_socket, combination)
+        # receive
+        recvmessage(client_socket)
 
-        
+        while True:
+            choice = input()
+            # print(f"choice is {choice}")
+            match int(choice):
+                case 1:
+                    sendmessage(client_socket, choice)
+                    recvmessage(client_socket)
+                    upload(client_socket)
+                case 2:
+                    sendmessage(client_socket, choice)
+                    recvmessage(client_socket)
+                case 3:
+                    stamp(f"Disconnected from server at {host}:{port}")
+                    break
+                case _:
+                    print("Invalid choice.")
 
         # Step 4: Receive the server's response
-        data = client_socket.recv(1024)  # Buffer size is 1024 bytes
-        if not data:
-            stamp("No response received from the server.")
-            raise ValueError("No response received from the server.")
-        stamp(f"Received from server: {data.decode()}")
 
     except Exception as e:
         stamp(f"Error: {e}")
